35dd343bcfb89155b99846160b3c815effae949c
Scott-Bunting <scott.bunting15@imperial.ac.uk>
2018-12-04 22:55:04 +0000

    Update:Create and Write to data_log.csv

--- bme680Control.py
+++ bme680Control.py
@@ -1,7 +1,13 @@
 import bme680
 import time
+import os
+from datetime import datetime
 
 sensor = bme680.BME680()
+
+path = "/home/pi/SIOT/data_log.csv"
+file = open(path,"a+")
+i=0
 
 sensor.set_humidity_oversample(bme680.OS_2X)
 sensor.set_pressure_oversample(bme680.OS_4X)
@@ -10,8 +16,17 @@ sensor.set_temperature_oversample(bme680.OS_8X)
 sensor.set_filter(bme680.FILTER_SIZE_3)
 
 while True:
-    if sensor.get_sensor_data():
-        output = "{0:.2f} C,{1:.2f},{2:.2f} %RH".format(sensor.data.temperature, sensor.data.pressure, sensor.data.humidity)
+    if sensor.get_sensor_data():	
+        output = "{0:.2f} C,{1:.2f} hPA,{2:.2f} %RH".format(sensor.data.temperature, sensor.data.pressure, sensor.data.humidity)
+        
         print(output)
+	
+        now = datetime.now()
+        file.write(str(now)+"," + output + "\n")
+        file.flush()
+        time.sleep(1)
+#        file.close()	
 
-    time.sleep(1)
+#time.sleep(1)
+
+file.close()
