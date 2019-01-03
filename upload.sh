654c03eef40a6b6513ac975cc241ee8d0555858b
Scott-Bunting <scott.bunting15@imperial.ac.uk>
2019-01-03 14:15:07 +0000

    Routine commit for data upload

--- upload.sh
+++ upload.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+#!/bin/expect
+#!/bin/send
+#!/bin/spawn
 
-git pull origin master
+spawn git pull origin master
+expect "Username for 'https://github.com':"         # Wait/expect for the Password string from the spawned process
+send "Scott-Bunting\r"      # send the password to spawned process as an input.
+expect "Password for 'https://Scott-Bunting@github.com':"
+send "Jaguar2018\r"
 git add .
 git commit -m "Routine commit for data upload"
 spawn git push origin master
